1f51c3149027ba3a09377ea2cb78b3abf2ba4443
Xi-Yuer <2214380963@qq.com>
2023-09-28 11:10:09 +0800

    config

--- models/user/index.go
+++ models/user/index.go
@@ -2,24 +2,25 @@ package models
 
 import (
 	"GinChat/utils"
+	"time"
 
 	"gorm.io/gorm"
 )
 
 type UserBasic struct {
 	gorm.Model
-	Name          string `json:"name" form:"name" binding:"required"`
-	Password      string `json:"password" form:"password" binding:"required"`
-	Phone         string `json:"phone" form:"phone" binding:"required"`
-	Email         string `json:"email" form:"email" binding:"required"`
-	ClientIP      string `json:"clientIP" form:"clientIP" binding:"required"`
-	Identity      string `json:"identity" form:"identity" binding:"required"`
-	ClientPort    string `json:"clientPort" form:"clientPort" binding:"required"`
-	LoginTime     string `json:"loginTime" form:"loginTime"`
-	LoginOutTime  string `json:"loginOutTime" form:"loginOutTime"`
-	HeartbeatTime string `json:"heartbeatTime" form:"heartbeatTime"`
-	IsLogout      bool   `json:"isLogout" form:"isLogout"`
-	DeviceInfo    string `json:"deviceInfo" form:"deviceInfo"`
+	Name          string    `json:"name" form:"name" binding:"required"`
+	Password      string    `json:"password" form:"password" binding:"required"`
+	Phone         string    `json:"phone" form:"phone" binding:"required"`
+	Email         string    `json:"email" form:"email" binding:"required"`
+	ClientIP      string    `json:"clientIP" form:"clientIP" binding:"required"`
+	Identity      string    `json:"identity" form:"identity" binding:"required"`
+	ClientPort    string    `json:"clientPort" form:"clientPort" binding:"required"`
+	LoginTime     time.Time `json:"loginTime" form:"loginTime"`
+	LoginOutTime  time.Time `json:"loginOutTime" form:"loginOutTime"`
+	HeartbeatTime time.Time `json:"heartbeatTime" form:"heartbeatTime"`
+	IsLogout      bool      `json:"isLogout" form:"isLogout"`
+	DeviceInfo    string    `json:"deviceInfo" form:"deviceInfo"`
 }
 
 // 表名
